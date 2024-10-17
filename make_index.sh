70508efa3b3e606d8281079e603ab60d89547aaa
Silas Brack <silasbrack@gmail.com>
2024-10-17 17:43:42 +0200

    Remove stupid shell script

--- make_index.sh
+++
@@ -1,2 +0,0 @@
-(find posts/* -maxdepth 1 | while read -r file; do echo "<a href=\""$file"\">"$(cat $file | grep -o '<title>.*</title>' | sed 's/\(<title>\|<\/title>\)//g')"</a>"; done) | grep -o '<a href="posts/[^(?!_)].*">.\{1,\}</a>' | sed 's/"/\\"/g' | awk '{ printf("%s<br/>", $0) }' | xargs -I '{}' sed -e 's|{{ INDEX }}|{}|g' index_template.html
-
