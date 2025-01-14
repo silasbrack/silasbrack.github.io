7ce115a1067cc546617c609b074d722506936a8a
Silas Brack <silasbrack@gmail.com>
2025-01-14 18:27:31 +0100

    add flake.nix for development environment

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My gleam monorepo";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.x86_64-linux.default = {
+          buildInputs = with pkgs; [
+            prettierd
+          ];
+        };
+      }
+    ));
+}
